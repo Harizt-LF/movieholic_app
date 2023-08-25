213a887b142a09d9be25b766def08421dd4796f8
Harizt-LF <hariztfajree@gmail.com>
2023-08-25 23:49:16 +0700

    Appbar Added

--- lib/main.dart
+++ lib/main.dart
@@ -9,12 +9,15 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-          body: Center(
-        child: Text("Halo"),
-      )),
+      home: SafeArea(
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text("Halo"),
+          ),
+        ),
+      ),
     );
   }
 }
