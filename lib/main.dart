6f962f6449bddd2d672e96f9339c53ab3aef4c83
hariztt-LF <hariztfajree@gmail.com>
2023-08-28 15:21:11 +0700

    added search bar

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MainApp extends StatelessWidget {
       home: SafeArea(
         child: Scaffold(
           appBar: AppBar(
+            centerTitle: true,
             toolbarHeight: 50,
             leading: Container(
               width: 100,
@@ -21,29 +22,46 @@ class MainApp extends StatelessWidget {
               //color: Colors.amber,
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.center,
-                children: [Icon(Icons.access_alarms)], //Test Icon
+                children: [Icon(Icons.house_siding_outlined)], //Test Icon
               ),
             ),
-            title: const Text("Halo Ini Aplikasi"),
+            title: const Text("Movielic"),
           ),
           body: Padding(
-              padding: const EdgeInsets.all(8.0),
+              padding: const EdgeInsets.symmetric(horizontal: 19),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   SizedBox(
                     height: 20,
                   ),
-                  Text("Halo"),
+                  Text(
+                    "Halo, User !",
+                    style: const TextStyle(
+                        fontWeight: FontWeight.bold, fontSize: 20),
+                  ),
                   SizedBox(
                     height: 20,
                   ),
                   Container(
-                    padding: EdgeInsets.symmetric(horizontal: 20),
-                    height: 30,
-                    color: Colors.amber,
+                    decoration: BoxDecoration(
+                        color: const Color.fromARGB(255, 243, 241, 235),
+                        border: Border.all(width: 1),
+                        borderRadius: BorderRadius.circular(10)),
+                    padding: EdgeInsets.symmetric(horizontal: 10),
+                    height: 45,
                     child: Row(
-                      children: [Icon(Icons.abc_sharp)],
+                      children: [
+                        Icon(Icons.search),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Text(
+                          "Cari movie favorit mu disini !",
+                          style:
+                              TextStyle(color: Colors.black.withOpacity(0.5)),
+                        )
+                      ],
                     ),
                   )
                 ],
