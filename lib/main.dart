e8398be7d950e7aca1507ab09aca33ee5d041e53
hariztt-LF <hariztfajree@gmail.com>
2023-08-28 15:00:11 +0700

    add body

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MainApp extends StatelessWidget {
       home: SafeArea(
         child: Scaffold(
           appBar: AppBar(
+            toolbarHeight: 50,
             leading: Container(
               width: 100,
               height: 100,
@@ -25,6 +26,28 @@ class MainApp extends StatelessWidget {
             ),
             title: const Text("Halo Ini Aplikasi"),
           ),
+          body: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Text("Halo"),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Container(
+                    padding: EdgeInsets.symmetric(horizontal: 20),
+                    height: 30,
+                    color: Colors.amber,
+                    child: Row(
+                      children: [Icon(Icons.abc_sharp)],
+                    ),
+                  )
+                ],
+              )),
         ),
       ),
     );
